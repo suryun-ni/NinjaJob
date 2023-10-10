69b86961329d0efb6a343951ec57e3be43256af4
suryun-ni <tengkusurya29@gmail.com>
2023-10-10 21:24:11 +0700

    tugas 06

---
+++ tugas-reactjs/src/App.js
@@ -0,0 +1,17 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="container-list">
+          <h1>Hi, Saya Tengku</h1>
+        </div>
+        <p>Saya dari Batch 50 sudah berhasil menginstall React</p>
+      </div>
+    </div>
+  );
+};
+
+export default App;
